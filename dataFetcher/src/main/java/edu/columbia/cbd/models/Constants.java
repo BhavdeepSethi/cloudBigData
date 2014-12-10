0c8413fbff98f60660f48e889270abb15a5ba623
diwakar21 <diwakar.mahajan@gmail.com>
2014-12-10 00:23:14 -0500

    alchemy constants

--- dataFetcher/src/main/java/edu/columbia/cbd/models/Constants.java
+++ dataFetcher/src/main/java/edu/columbia/cbd/models/Constants.java
@@ -7,6 +7,8 @@ public class Constants {
 
     public static String TWITTER_QUEUE_NAME = "twitterCBD";
     public static String TWITTER_QUEUE_URL = "";
+    public static String ALCHEMY_URL="http://access.alchemyapi.com/calls/text/TextGetTextSentiment";
+    public static String ALCHEMY_API_KEY="5f58362ae76b9330067f711a854d7d2e82f20b46";
 
     public static String[] KEYWORDS;
 }
